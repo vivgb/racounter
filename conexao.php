ea2ebd746e139fbbcc004a5c8f4ad5e595231627
vivgb <vitoriagabrieli87@gmail.com>
2025-04-30 21:04:43 -0300

    Update conexao.php
    nome

--- conexao.php
+++ conexao.php
@@ -1,6 +1,6 @@
 <?php
 
     //Abrir conexão com o Banco de Dados
-    $conn = mysqli_connect("localhost","root","","sistemas_contagem") or die("Falha: ". mysqli_connect_error());
+    $conn = mysqli_connect("localhost","root","","sistema_contagem") or die("Falha: ". mysqli_connect_error());
 
 ?>
