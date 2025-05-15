2b157e407d07599c6d292009f596080972daecae
euthgz <thiagosp419@gmail.com>
2025-05-14 21:18:30 -0300

    conexão com o banco de dados feita

--- php/validaLogin.php
+++ php/validaLogin.php
@@ -8,28 +8,43 @@
     $_SESSION['logado'] = 0;
 
     $email = stripslashes($_POST["nEmail"]);
-    $senha =  ($_POST["nSenha"]);
+    $senha = stripslashes($_POST["nSenha"]);
+
+    //$_POST - Valor enviado pelo FORM através da propriedade NAME do elemento HTML 
+    //$_GET - Valor enviado pelo FORM através da URL
+    //$_SESSION - Variável criada pelo usuário no PHP
 
     include("conexao.php");
-    
     $sql = "SELECT * FROM usuarios "
             ." WHERE email = '$email' "
             ." AND senha = md5('$senha');";
-            
     $resultLogin = mysqli_query($conn,$sql);
     mysqli_close($conn);
 
+    //Validar se tem retorno do BD
     if (mysqli_num_rows($resultLogin) > 0) {  
+        
+        //enviarEmail('destino@email.com.br','Mensagem de e-mail para SA','Teste SA','Eu mesmo');
+
         foreach ($resultLogin as $coluna) {
-            $_SESSION['id_tipo_usuario'] = $coluna['id_tipo_usuario'];
+                        
+            //***Verificar os dados da consulta SQL
+            $_SESSION['idTipoUsuario'] = $coluna['id_tipo_usuario'];
             $_SESSION['logado']        = 1;
             $_SESSION['idLogin']       = $coluna['id_usuario'];
             $_SESSION['NomeLogin']     = $coluna['nome'];
+            $_SESSION['FotoLogin']     = $coluna['Foto'];
             $_SESSION['AtivoLogin']    = $coluna['flg_ativos'];
 
+            //Acessar a tela inicial
             header('location: ../painel.php');
-        }
-    }else {
+            
+        }        
+    }else{
+        //Acessar a tela inicial
         header('location: ../');
-    }
+    } 
+
+    
+
 ?>
