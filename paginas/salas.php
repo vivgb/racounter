e208aed57ef45151c201e5b26aaa01ce52bb4071
amanda <amndgr.2022@gmail.com>
2025-05-27 15:06:31 -0300

    c

--- paginas/salas.php
+++ paginas/salas.php
@@ -19,22 +19,19 @@ if (basename(__FILE__) === basename($_SERVER['SCRIPT_FILENAME'])) {
       <p>Lotação 2/30</p>
       <p>agendado</p>
     </div>
-  <div class="grid-container">
     <div class="card">
       <h2>Sala 102</h2>
       <p>Lotação 5/30</p>
     </div>
-  <div class="grid-container">
     <div class="card">
       <h2>Sala 103</h2>
       <p>Lotação 2/30</p>
     </div>
-  <div class="grid-container">
     <div class="card">
       <h2>Sala 104</h2>
       <p>Lotação 8/35</p>
       <p>agendado</p>
     </div>
-    </div>
+  </div>
   
 </section>
