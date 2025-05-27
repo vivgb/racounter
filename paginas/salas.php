a6077139ef19878b8e0bcb866e5f00f571d10f54
amandagr <amndgr.2022@gmail.com>
2025-05-27 19:20:47 -0300

    ajustes

--- paginas/salas.php
+++ paginas/salas.php
@@ -11,30 +11,27 @@ if (basename(__FILE__) === basename($_SERVER['SCRIPT_FILENAME'])) {
 <section id="salas">
   <h1>Salas</h1>
   <div class="grid-container">
-    <div class="card" id="novaSala">
-      <i class='bxr  bx-plus'></i> 
-    </div>
-    <div class="card">
-      <h2>Sala 101</h2>
-      <p>Lotação 2/30</p>
-      <p>agendado</p>
-    </div>
-  <div class="grid-container">
-    <div class="card">
-      <h2>Sala 102</h2>
-      <p>Lotação 5/30</p>
-    </div>
-  <div class="grid-container">
-    <div class="card">
-      <h2>Sala 103</h2>
-      <p>Lotação 2/30</p>
-    </div>
-  <div class="grid-container">
-    <div class="card">
-      <h2>Sala 104</h2>
-      <p>Lotação 8/35</p>
-      <p>agendado</p>
-    </div>
+      <div class="card" id="novaSala">
+        <i class='bxr  bx-plus'></i> 
+      </div>
+      <div class="card" onclick="redirectJS('<?php echo $id;?>')">
+        <h2>Sala 101</h2>
+        <p>Lotação 2/30</p>
+        <p>agendado</p>
+      </div>
+      <div class="card">
+        <h2>Sala 102</h2>
+        <p>Lotação 5/30</p>
+      </div>
+      <div class="card">
+        <h2>Sala 103</h2>
+        <p>Lotação 2/30</p>
+      </div>
+      <div class="card">
+        <h2>Sala 104</h2>
+        <p>Lotação 8/35</p>
+        <p>agendado</p>
+      </div>
     </div>
   
 </section>
