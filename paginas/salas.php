d5793962c8c9c88eaa0378372848843325e94184
amandagr <amndgr.2022@gmail.com>
2025-05-26 22:13:10 -0300

    k

--- paginas/salas.php
+++ paginas/salas.php
@@ -2,7 +2,7 @@
 
 <?php
 // Evita acesso direto
-if (basename(_FILE_) === basename($_SERVER['SCRIPT_FILENAME'])) {
+if (basename(__FILE__) === basename($_SERVER['SCRIPT_FILENAME'])) {
     header("Location: ../index.php");
     exit;
 }
