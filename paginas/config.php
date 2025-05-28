6e0fc7c20213b27ddcc1d779fd2e087c5d902e95
Gabriel Arêas <119638298+GabrielTvOF@users.noreply.github.com>
2025-05-27 21:15:09 -0300

    config

--- paginas/config.php
+++ paginas/config.php
@@ -6,7 +6,7 @@ if (basename(__FILE__) === basename($_SERVER['SCRIPT_FILENAME'])) {
     exit;
 }
 ?>
-<link rel="stylesheet" href="css/config.css">
+<link rel="stylesheet" href="css/style.css">
 <section id="Settings">
     <div class="head-title">
         <div class="left">
