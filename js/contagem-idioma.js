3c81df8e7eed4449cd0e6438b43abb900fd33fce
Gabriel Arêas <119638298+GabrielTvOF@users.noreply.github.com>
2025-06-02 19:26:34 -0300

    idiomas?????

---
+++ js/contagem-idioma.js
@@ -0,0 +1,33 @@
+const translations = {
+    "pt-BR": {
+      pageTitle: "Contagem de Pessoas na Sala",
+      currentCapacity: "Lotação atual",
+      maxCapacity: "Lotação máxima",
+      status: "Status",
+      occupied: "Ocupada",
+      free: "Livre",
+      roomNotFound: "Sala não encontrada.",
+      noRoomId: "ID da sala não fornecido."
+    },
+    "en": {
+      pageTitle: "Room Occupancy Count",
+      currentCapacity: "Current Capacity",
+      maxCapacity: "Maximum Capacity",
+      status: "Status",
+      occupied: "Occupied",
+      free: "Free",
+      roomNotFound: "Room not found.",
+      noRoomId: "Room ID not provided."
+    },
+    "es": {
+      pageTitle: "Conteo de Personas en la Sala",
+      currentCapacity: "Capacidad actual",
+      maxCapacity: "Capacidad máxima",
+      status: "Estado",
+      occupied: "Ocupada",
+      free: "Libre",
+      roomNotFound: "Sala no encontrada.",
+      noRoomId: "ID de sala no proporcionado."
+    }
+  };
+  
