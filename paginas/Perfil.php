9537dd9abe1b67260836d1b5aa5d0eedb5dca242
Gabriel Arêas <119638298+GabrielTvOF@users.noreply.github.com>
2025-06-04 21:41:01 -0300

    21321321

--- paginas/Perfil.php
+++ paginas/Perfil.php
@@ -1,4 +1,6 @@
-<section class="container" role="main" aria-label="Tela de Perfil de Usuário">
+<div class="gordao">
+
+  <section class="gordo" role="main" aria-label="Tela de Perfil de Usuário">
     <h1>Perfil do Usuário</h1>
     <div class="profile-photo" aria-label="Foto do perfil">
       <img src="https://i.pravatar.cc/140" alt="Foto do perfil do usuário" id="photo" />
@@ -7,17 +9,19 @@
         <button id="changePhotoBtn" aria-label="Alterar foto do perfil">Alterar Foto</button>
       </form>
     </div>
-
+    
     <div class="info-section" aria-label="Informações do usuário">
       <div class="info-box" aria-labelledby="emailTitle">
         <h2o id="emailTitle">E-mail</h2o>
         <p class="info-text" id="email"><?php echo $_SESSION['E-mailLogin']?></p>
       </div>
-
+      
       <div class="info-box" aria-labelledby="personalDataTitle" style="grid-column: span 2;">
         <h2o id="personalDataTitle">Dados Pessoais</h2o>
         <p class="info-text"><strong>Nome:</strong><?php echo $_SESSION['NomeLogin']?></p>
         <p class="info-text"><strong>Telefone:</strong><?php echo $_SESSION['telLogin']?></p>
         <p class="info-text"><strong>Data de Nascimento:</strong><?php echo $_SESSION['dataNascLogin']?></p>
-
-  </saction>
+        
+      
+  </section>
+</div>
